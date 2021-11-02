916195eb6df7e80e632db637b930fa9631606549
fan <fw@bk.ru>
2021-11-02 17:29:55 +0300

    add options

--- selenium_test.py
+++ selenium_test.py
@@ -7,22 +7,27 @@ import time
 
 
 
-url='https://юзерагент.рф/'
+#url='https://юзерагент.рф/'
+url='https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html'
 chrome_path='/home/fan/projects/chromedriver' #WINDOWS->   c:\\driver\\selenium.exe
 user_agent=UserAgent()
+
+
 
 
 options = webdriver.ChromeOptions()
 #options.add_argument('headless')
 options.add_argument(f'user-agent={user_agent.random}')
 
+# for ChromeDriver version 79.0.3945.16 or over
+options.add_argument("--disable-blink-features=AutomationControlled")
 
 
 browser = webdriver.Chrome(executable_path=chrome_path, options=options)
 
 try:
     browser.get(url=url)
-    time.sleep(2)
+    time.sleep(3)
 except Exception as ex:
     print(ex)
 finally:
